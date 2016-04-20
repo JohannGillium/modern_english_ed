01ce08584871f055ce11553811bb500a189a276c
JohannGillium <johanngillium@gmail.com>
2016-04-20 13:15:57 -0400

    Now the script pu all the texts in the same repository. What remains is mostly enhancement of the XSLT

--- Python/main.py
+++ Python/main.py
@@ -5,17 +5,20 @@ import lxml.etree as etree
 
 #Need to create here some global variable 
 
-for i in os.listdir('/home/jgillium/Bureau/Corpus_modern_english/sample'):
+for i in os.listdir('/home/jgillium/Bureau/Corpus_modern_english/modern_english/uvaGenText/tei'):
     xslt_path = '/home/jgillium/Bureau/Ed_modern_english/XSLT/modern_english_corpus.xsl'
-    folder_path = '/home/jgillium/Bureau/Corpus_modern_english/sample' 
+    folder_path = '/home/jgillium/Bureau/Corpus_modern_english/modern_english/uvaGenText/tei' 
     name_file_without_extension = os.path.splitext(i)[0]
-    folder_mdf = os.path.join('/home/jgillium/Bureau/Ed_modern_english/Output',name_file_without_extension,'_texts')
+#Below the folder_mdf variable for one ed repository for each text
+#    folder_mdf = os.path.join('/home/jgillium/Bureau/Ed_modern_english/Output',name_file_without_extension,'_texts')
+    folder_mdf = os.path.join('/home/jgillium/Bureau/Ed_modern_english/Output/ed/_texts')
     name_file_without_extension = os.path.splitext(i)[0]
     name_markdown_file = name_file_without_extension+'.md'
     print name_markdown_file
     path_output = os.path.join(folder_mdf,name_markdown_file)                 
     xml_path = os.path.join(folder_path,i)
-    ed_repository_creation = start_ed(name_file_without_extension)
+#Below the code to create one ed repository for each text
+#    ed_repository_creation = start_ed(name_file_without_extension)
     markdown_file_creation = process_xml(name_file_without_extension, xml_path, xslt_path, path_output)
     
 
